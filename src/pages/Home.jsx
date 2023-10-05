76e845d2fdaae5ddf6c72ec2bce3819131f0a315
Akash Singh <akashkumarsingh11032001@gmail.com>
2023-10-05 23:20:49 +0530

    components creation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Headers from '../components/Headers'
+import Info from '../components/Info'
+
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+        <Headers />
+        <Info />
+    </div>
   )
 }
 
