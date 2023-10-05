549ba7fde2570a78a3c2c932f2da890dd17ef655
Akash Singh <akashkumarsingh11032001@gmail.com>
2023-10-05 23:27:26 +0530

    projects structure

--- src/components/Headers.jsx
+++ src/components/Headers.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Headers = () => {
   return (
-    <div>Headers</div>
+    <Box>
+        
+    </Box>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import Headers from '../components/Headers'
 import Info from '../components/Info'
-
+import Section from '../components/Section'
+import Products from '../components/Products'
 
 const Home = () => {
   return (
     <div>
         <Headers />
         <Info />
+        <Section />
+        <Section />
+        <Section />
+        <Products />
     </div>
   )
 }
