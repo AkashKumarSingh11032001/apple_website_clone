27907045767a7a3588bcaafcd4097ea50a931901
Akash Singh <akashkumarsingh11032001@gmail.com>
2023-10-06 05:51:44 +0530

    Header section

--- src/components/Headers.jsx
+++ src/components/Headers.jsx
@@ -1,12 +1,45 @@
-import { Box } from '@mui/material'
-import React from 'react'
+import React from "react";
+import SearchIcon from "@mui/icons-material/Search";
+import WorkOutlineIcon from "@mui/icons-material/WorkOutline";
+import { appleLogo, navData } from "../constants/constant";
+import { Box, Typography, styled } from "@mui/material";
+
+const Component = styled(Box)({
+  backgroundColor: "#f5f5f7",
+  height: 44,
+});
+
+const NavBar = styled(Box)({
+  display: "flex",
+  width: "100%",
+  height: "100%",
+  justifyContent: "center",
+  alignItems: "center",
+  margin: "0 auto",
+  "& > *": {
+    color: "rgba(0, 0, 0, .8)",
+    fontWeight: 600,
+    opacity: 0.8,
+    padding: "0 21px",
+    fontSize: 12,
+  },
+});
 
 const Headers = () => {
   return (
-    <Box>
+    <Component>
+      <NavBar>
+        <img src={appleLogo} alt="logo" style={{ width: 15 }} />
+        {navData.map((nav) => (
+          <Typography style={{ fontSize: 12, fontWeight: 600 }}>
+            {nav}
+          </Typography>
+        ))}
+        <SearchIcon fontSize="small" />
+        <WorkOutlineIcon fontSize="small" />
+      </NavBar>
+    </Component>
+  );
+};
 
-    </Box>
-  )
-}
-
-export default Headers
+export default Headers;
