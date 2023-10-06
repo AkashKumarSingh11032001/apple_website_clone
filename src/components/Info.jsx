6ff9e0d43e776e57b537ceee8a91c807bbd3bf19
Akash Singh <akashkumarsingh11032001@gmail.com>
2023-10-06 05:54:52 +0530

    info section done

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import { Typography, Box, styled } from "@mui/material";
+
+const Component = styled(Box)({
+  backgroundColor: "#fff",
+  display: "flex",
+  justifyContent: "center",
+  "& > p": {
+    fontSize: 14,
+    padding: "12px 0",
+    color: "#1d1d1f",
+  },
+});
 
 const Info = () => {
   return (
-    <div>Info</div>
-  )
-}
+    <Component>
+      <Typography>
+        Get up to ₹8000.00 instant savings on selected products with eligible
+        HDFC Bank cards.*{" "}
+        <span style={{ color: "#2997ff" }}>Shop now &gt;</span>
+      </Typography>
+    </Component>
+  );
+};
 
-export default Info
+export default Info;
