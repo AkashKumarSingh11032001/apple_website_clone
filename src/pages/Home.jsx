9ab30783100efbca4e3298f48ed2cc40228a27a1
Akash Singh <akashkumarsingh11032001@gmail.com>
2023-10-06 06:05:30 +0530

    section componenets done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,15 +3,16 @@ import Headers from '../components/Headers'
 import Info from '../components/Info'
 import Section from '../components/Section'
 import Products from '../components/Products'
+import { products ,productsSection} from '../constants/constant'
 
 const Home = () => {
   return (
     <div>
         <Headers />
         <Info />
-        <Section />
-        <Section />
-        <Section />
+        <Section data={productsSection.iphonePro}/>
+        <Section data={productsSection.iphone}/>
+        <Section data={productsSection.macbookpro}/>
         <Products />
     </div>
   )
